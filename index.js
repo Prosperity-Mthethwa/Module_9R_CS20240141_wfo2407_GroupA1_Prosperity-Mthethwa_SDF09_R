12c6ba258e5c28c5d915cdd3f2e96284566c6b89
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-08-01 00:16:55 +0200

    modified index html and js files

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ let sum = firstCard + secondCard
 let hasBlackJack = false 
 let isAlive = true 
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
+console.log(sumEl)
 
 function startGame() {
+    sumEl.textContent = "sum: " +  sum
     if (sum <= 20) {
-            message = "Do you want to draw a new card🙂?"
+            message = "Do you want to draw a new card?"
         } else if (sum === 21) {
-            message = "Wohoo! You've got Blackjack🥳"
+            message = "You've got Blackjack"
             hasBlackJack = true
         } else {
-            message = "You're out of the game😭"
+            message = "You're out of the game"
             isAlive = false
         }
 
-        console.log(message)
+        messageEl.textContent = message
 }

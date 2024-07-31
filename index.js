38bf6db57deac6ed7a83bd09d13a768cf2716da5
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-07-31 23:58:57 +0200

    modified html, css and js files

--- index.js
+++ index.js
@@ -3,23 +3,23 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard 
 let hasBlackJack = false 
 let isAlive = true 
-
 let message = ""
 
-if (sum <= 21) 
-{
-    message = "Do you want to draw a new card🙂?"
-} else if (sum === 21) 
-{
-    message = "Wohoo! You've got Blackjack🥳"
-    hasBlackJack = true
-} else (sum > 21) 
-{
-    message = "You're out of the game😭"
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+            message = "Do you want to draw a new card🙂?"
+        } else if (sum === 21) {
+            message = "Wohoo! You've got Blackjack🥳"
+            hasBlackJack = true
+        } else {
+            message = "You're out of the game😭"
+            isAlive = false
+        }
+
+        console.log(message)
 }
